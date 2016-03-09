2f1165c12769c81abedb9e0126820a7dfa1585d4
Jan Sabsch <sabsch@st.ovgu.de>
2016-03-09 15:24:45 +0100

    Implement initial navigator structure. Implement basic state machine and an inherited version for our problem.

--- EV3/main.py
+++ EV3/main.py
@@ -3,11 +3,12 @@
 
 from rfcomm_server import RFCOMMServer
 from driver import Driver
+from navigator import Navigator
 import time
-
 
 LOOP_DURATION = 0.01  # in s
 ok = True
+bt_data = 0
 
 print "Welcome!"
 
@@ -16,18 +17,19 @@ btserver = RFCOMMServer()
 btserver.wait_for_connection()
 print "Connected!"
 
-
 driver = Driver(LOOP_DURATION)
-data = 0
+nav = Navigator()
 
 while ok:
     start = time.time()
-    driver.move()  #no code before this point!
+    ok = ok and driver.move()  #no code before this point!
     
     # TODO: here comes the other code
+    ok = ok and nav.get_bt(bt_data)
+    ok = ok and driver.give_command(nav.find_commands())
     
     
-    data = btserver.wait_for_data() # no code after this point!
+    bt_data = btserver.wait_for_data() # no code after this point!
     
     end = time.time()
     remain = start + LOOP_DURATION - end
